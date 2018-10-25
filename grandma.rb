e27778216570eaa761457304578bbcc88e231946
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-25 04:30:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
 string = "I LOVE YOU GRANDMA!"
-string.new = "WHAT DID YOU EAT TODAY?"
 def speak_to_grandma(string)
 if (string == string.upcase)
   return "I LOVE YOU TOO PUMPKIN!"
-else (string.new == string.new.downcase)
+else (string == string.downcase)
   return "HUH?! SPEAK UP, SONNY!"
-if (string == string.upcase)
+if (string == false)
   return "NO, NOT SINCE 1938!"
 else
   return "NO, NOT SINCE 1938!"
