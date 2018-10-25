4aa681c0fe8d0765327a3f543559f525bd6cbc01
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-25 04:40:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ if (string == string.upcase)
   return "I LOVE YOU TOO PUMPKIN!"
 else (string == string.downcase)
   return "HUH?! SPEAK UP, SONNY!"
-if (string == !false)
+if phrase.upcase
   return "NO, NOT SINCE 1938!"
 else
   return "NO, NOT SINCE 1938!"
