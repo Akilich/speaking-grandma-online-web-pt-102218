a0d03b0ad980fd178514a259253bb2502b0f4911
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-25 04:11:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
-string = "I LOVE YOU GRANDMA!"
-phrase = "WHAT DID YOU EAT TODAY?"
+string = "I LOVE YOU GRANDMA!" phrase = "WHAT DID YOU EAT TODAY?"
+
 def speak_to_grandma(string)
 if (string == string.upcase)
   return "I LOVE YOU TOO PUMPKIN!"
