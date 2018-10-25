2da4e5ae7ed0aca16b874f40040017dc65de8f0b
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-25 05:03:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,11 @@
 string = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(string)
-if string.downcase
-  return "HUH?! SPEAK UP, SONNY!"
-def speak_to_grandma(string)
 if string.upcase
-  return "NO, NOT SINCE 1938!"
-else (string == string.upcase)
   return "I LOVE YOU TOO PUMPKIN!"
+else string.downcase
+  return "HUH?! SPEAK UP, SONNY!"
+else
+  return "NO, NOT SINCE 1938!"
 end 
 end
 end
