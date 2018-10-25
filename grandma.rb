0c1b663a6796951c9f8d2a175a2a052583757d5a
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-25 04:01:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,9 @@ if (string == string.upcase)
   return "I LOVE YOU TOO PUMPKIN!"
 else (string == string.downcase)
   return "HUH?! SPEAK UP, SONNY!"
-else string == "WHAT DID YOU EAT TODAY?"
+elsif string == "WHAT DID YOU EAT TODAY?"
 return "NO, NOT SINCE 1938!"
 end 
+end
 end
 end
